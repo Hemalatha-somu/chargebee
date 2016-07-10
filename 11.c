04e5caf943749be375f9b2da305dbe39c1bd8c0b
Hemalatha-somu <heme96.hs@gmail.com>
2016-07-10 11:25:28 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -7,9 +7,9 @@ printf("\n Enter the n value:");
 scanf("%d",&n);
 printf("\n Enter the pair");
 scanf("%d",&p);
-for(i=0;i<2*n1;i++)
+for(i=0;i<2*n;i++)
 {
-x=2*n1-i;
+x=2*n-i;
 if(p!=x)
 {
 printf("The pair is %d for days %d is %d\n",p,j++,p);
